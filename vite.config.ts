7921169978cb95f95098897fca3f5a0f3e82f9c0
JannePiekkola <janne.piekkola@kuuasema.com>
2026-05-01 16:50:37 +0300

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Chain3D_Urban-Occult/",
+  base: '/Chain3D_Urban-Occult/',
+  server: { port: 5173 }
 });
